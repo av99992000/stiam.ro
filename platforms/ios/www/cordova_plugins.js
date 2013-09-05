c3f4785c6278c2667d92817eeb718b0d65b1e693
Alin Voinea <alin@eaudeweb.ro>
2013-09-05 21:41:42 +0300

    Add ios platform

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,11 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.core.splashscreen/www/splashscreen.js",
+        "id": "org.apache.cordova.core.splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    }
+]
+});
