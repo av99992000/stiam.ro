2f620f43b11d052d3706be5099bc4af4d9f7d21f
Alin Voinea <alin.voinea@gmail.com>
2015-02-03 23:43:43 +0200

    Cleanup

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/joint/android/sandbox/stiam.ro/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/joint/android/sandbox/stiam.ro/platforms/android/CordovaLib/AndroidManifest.xml \
--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/joint/android/sandbox/stiam.ro/platforms/android/ant-build/AndroidManifest.xml : \
-/joint/android/sandbox/stiam.ro/platforms/android/AndroidManifest.xml \
